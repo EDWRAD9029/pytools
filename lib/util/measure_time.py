1be08fc1ae862d2b8dd294707320cda45fd23ef5
EDWRAD9029 <106041077+EDWRAD9029@users.noreply.github.com>
2022-05-26 23:25:47 +0900

    Add files via upload

--- lib/util/measure_time.py
+++ lib/util/measure_time.py
@@ -7,7 +7,7 @@ import time
 def print_process_time(f):
     """
     計測デコレータ
-    CPU使用時間を計測する
+    経過時間を計測する
     
     使用例：
     import measure_time
@@ -16,6 +16,11 @@ def print_process_time(f):
         // 計測されるコード
     """
     def print_process_time_func(*args, **kwargs):
+        # 引数表示
+        print("時間を計測します...")
+        print("args   :",args)
+        print("kwargs :",kwargs)
+        
         # 開始
         print("------------------------ 時間計測　開始 ------------------------")
         start_time = time.process_time()
@@ -23,14 +28,14 @@ def print_process_time(f):
         # 関数実行
         return_val = f(*args, **kwargs)
  
-        # 修了
+        # 終了
         end_time = time.process_time()
         print("------------------------ 時間計測　終了 ------------------------")
  
         # 関数名と経過時間を出力(秒)
         elapsed_time = end_time - start_time
-        print("function     : " + f.__name__)
-        print("elapsed_time : " + str(elapsed_time),"sec")
+        print("function     :", f.__name__)
+        print("elapsed_time :", "{:.3f}".format(elapsed_time),"sec")
  
         # 戻り値を返す
         return return_val
@@ -51,6 +56,9 @@ def print_performance_time(f):
     """
     def print_performance_time_func(*args, **kwargs):
         # 引数表示
+        print("時間を計測します...")
+        print("args   :",args)
+        print("kwargs :",kwargs)
         
         # 開始
         print("------------------------ 時間計測　開始 ------------------------")
@@ -59,17 +67,28 @@ def print_performance_time(f):
         # 関数実行
         return_val = f(*args, **kwargs)
  
-        # 修了
+        # 終了
         end_time = time.perf_counter()
         print("------------------------ 時間計測　終了 ------------------------")
  
         # 関数名と経過時間を出力(秒)
         elapsed_time = end_time - start_time
-        print("function     : " + f.__name__)
-        print("elapsed_time : " + str(elapsed_time),"sec")
+        print("function     :", f.__name__)
+        print("elapsed_time :", "{:.3f}".format(elapsed_time),"sec")
  
         # 戻り値を返す
         return return_val
  
     return print_performance_time_func
 
+class Timer():
+    """
+    複数行にまたがる処理時間を計測する
+    """
+    def __init__(self):
+        self.time_start = None
+        self.time_finish = None
+        self.timer_mode = None
+    
+    def start(self):
+        pass
