71b6000aa9afa43b0611d20f04027cfe491efa5e
EDWRAD9029 <106041077+EDWRAD9029@users.noreply.github.com>
2022-05-28 23:29:08 +0900

    Add files via upload

--- lib/util/measure_time.py
+++ lib/util/measure_time.py
@@ -3,11 +3,12 @@
 
 """
 import time
+import numpy as np
 
 def print_process_time(f):
     """
     計測デコレータ
-    経過時間を計測する
+    CPU使用時間を計測する
     
     使用例：
     import measure_time
@@ -17,7 +18,7 @@ def print_process_time(f):
     """
     def print_process_time_func(*args, **kwargs):
         # 引数表示
-        print("時間を計測します...")
+        print("CPU使用時間を計測します...")
         print("args   :",args)
         print("kwargs :",kwargs)
         
@@ -46,7 +47,7 @@ def print_process_time(f):
 def print_performance_time(f):
     """
     計測デコレータ
-    CPU使用時間を計測する
+    経過時間を計測する
     
     使用例：
     import measure_time
@@ -56,7 +57,7 @@ def print_performance_time(f):
     """
     def print_performance_time_func(*args, **kwargs):
         # 引数表示
-        print("時間を計測します...")
+        print("経過時間を計測します...")
         print("args   :",args)
         print("kwargs :",kwargs)
         
@@ -86,29 +87,34 @@ class Timer():
     複数行にまたがる処理時間を計測する
     """
     def __init__(self):
-        self.time = []
-        self.time_start = None
-        self.time_finish = None
+        self.clear()
         self.timer_mode()
     
-    def timer_mode(self,func=time.process_time):
+    def timer_mode(self,func=time.perf_counter):
         self.timer_func = func
     
     def start(self):
         print("------------------------ 時間計測　開始 ------------------------")
         self.time_start = self.timer_func()
-        self.time.append(self.time_start)
+        self.times.append(self.time_start)
     
     def finish(self):
         self.time_finish = self.timer_func()
-        self.time.append(self.time_finish)
+        self.times.append(self.time_finish)
         elapsed_time = self.time_finish - self.time_start
         print("------------------------ 時間計測　終了 ------------------------")
         print("elapsed_time :", "{:.3f}".format(elapsed_time),"sec")
         return elapsed_time
     
     def lap(self):
-        self.time.append(self.timer_func())
+        self.times.append(self.timer_func())
     
     def print_times(self):
-        pass
+        for i,j in enumerate(np.diff(np.array(self.times))):
+            print("time"+str(i),"-","time"+str(i+1),":","{:.3f}".format(j),"sec")
+        return [np.diff(np.array(self.times))]
+    
+    def clear(self):
+        self.times = []
+        self.time_start = None
+        self.time_finish = None
