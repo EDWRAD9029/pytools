b29be59967236b4b9a327093e9c6dff990715de8
EDWRAD9029 <106041077+EDWRAD9029@users.noreply.github.com>
2022-06-04 02:24:23 +0900

    Add files via upload

--- lib/image/detect_str_from_image.py
+++ lib/image/detect_str_from_image.py
@@ -1,1 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
 
+"""
+
+from PIL import Image
+import pyocr
+
+class detect_str_from_image():
+    """
+    
+    """
+    def __init__(self):
+        pass
+    
+    def detect_str_from_image_using_OCR(self,file=None,lang="eng"):
+        # OCRエンジンを取得
+        engines = pyocr.get_available_tools()
+        engine = engines[0]
+        
+        # 画像の文字を読み込む
+        return engine.image_to_string(Image.open(file), lang=lang)
