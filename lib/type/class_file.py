74c33d1de27cd1ad9a96fa23db63572c56e323ee
EDWRAD9029 <106041077+EDWRAD9029@users.noreply.github.com>
2022-06-15 00:41:35 +0900

    Add files via upload

--- lib/type/class_file.py
+++ lib/type/class_file.py
@@ -24,15 +24,68 @@ class Files():
     def __init__(self):
         pass
     
+    
+    
     def set_dir(self,new_path=None):
+        """
+        絶対参照パスを保存する。
+        
+        パラメータ
+        ----------
+        new_path : str
+            ファイルの絶対参照パス。
+        
+        返り値
+        -------
+        None
+        
+        """
         self.filepath = new_path
     
+    
+    
     def select(self,filetype=None):
+        """
+        ファイルを選択し、絶対参照パスを保存する。
+        
+        パラメータ
+        ----------
+        filetype : str
+            ファイルの種類。指定しない場合は全てのファイルから選択できる。
+            大文字は全て小文字として判断される。
+            対応していない文字列の場合はエラーを表示する。
+            デフォルトは None。
+            ---------------------------------
+            |   filetype  |       対象       |
+            =================================
+            |     None    | 全てのファイル 複数  |
+            ---------------------------------
+            |     csv     | CSVファイル 1つ     |
+            ---------------------------------
+            |     csvs    | CSVファイル 複数   |
+            ---------------------------------
+            |  image,img  | 画像ファイル 1つ    |
+            ---------------------------------
+            | images,imgs | 画像ファイル 複数  |
+            ---------------------------------
+            |    movie    | 動画ファイル 1つ    |
+            ---------------------------------
+            |    movies   | 動画ファイル 複数  |
+            ---------------------------------
+            例："image"とすると、画像ファイル名のみ選択できるようになる。
+        
+        返り値
+        -------
+        None
+        
+        """
         selector = select_file.selector_files()
         if filetype == None:
-            self.set_dir(selector.select_file())
+            self.set_dir(selector.select_file(multiple=True))
         elif filetype.lower() in ["csv"]:
             self.set_dir(selector.select_file_csv())
+        elif filetype.lower() in ["csvs"]:
+            self.set_dir(selector.select_file_csv(multiple=True))
         elif filetype.lower() in ["image","img"]:
             self.set_dir(selector.select_file_image())
         elif filetype.lower() in ["images","imgs"]:
